66dc30fcd5949768b2e68df85af2599d50eeca70
AndresCdo <andrezz1997@gmail.com>
2022-09-13 13:52:23 -0500

    Main file updated.

--- main.tf
+++ main.tf
@@ -4,26 +4,107 @@ provider "google" {
   zone    = var.zone
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "e2-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  network_interface {
-    # A default network is created for all GCP projects
-    network = "default"
-    access_config {
-    }
-  }
+resource "google_project_service" "gcp_services" {
+  for_each = toset(var.gcp_service_list)
+  project = "dot-boom"
+  service = each.key
 }
 
 # Create a GCS Bucket
 resource "google_storage_bucket" "my_bucket" {
 name     = var.bucket_name
 location = var.region
+}
+
+resource "google_compute_network" "default" {
+  name = "test-network"
+}
+
+resource "google_compute_address" "static_1" {
+  name = "ipv4-address-1"
+}
+
+resource "google_compute_address" "static_2" {
+  name = "ipv4-address-2"
+}
+
+resource "google_compute_subnetwork" "default" {
+  name          = "my-subnet"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = var.region
+  network       = google_compute_network.default.id
+}
+
+resource "google_compute_address" "internal_with_subnet_and_address" {
+  name         = "my-internal-address"
+  subnetwork   = google_compute_subnetwork.default.id
+  address_type = "INTERNAL"
+  address      = "10.0.42.42"
+  region       = var.region
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = google_compute_network.default.name
+  target_tags   = ["allow-ssh"] // this targets our tagged VM
+  source_ranges = ["0.0.0.0/0"]
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "2022", "80", "443", "8080", "1000-2000", "6443"]
+  }
+
+  source_tags = ["web"]
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "${var.machine_type}"
+  tags         = ["allow-ssh"] // this receives the firewall rule
+
+  boot_disk {
+    initialize_params {
+      image = "${var.linux_image}"
+    }
+  }
+  
+  metadata = {
+    ssh-keys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network    = google_compute_network.default.name
+    subnetwork = google_compute_subnetwork.default.name 
+    access_config {
+      nat_ip = google_compute_address.static_1.address
+    }
+  }
+}  
+
+resource "google_compute_instance" "jenkins_instance" {
+  name         = "jenkins-instance"
+  machine_type = "${var.machine_type}"
+  tags         = ["allow-ssh"] // this receives the firewall rule
+
+  boot_disk {
+    initialize_params {
+      image = "${var.linux_image}"
+    }
+  }
+    
+  metadata = {
+    ssh-keys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
+  }
+
+  network_interface {
+    network    = google_compute_network.default.name
+    subnetwork = google_compute_subnetwork.default.name
+    access_config {
+      nat_ip = google_compute_address.static_2.address
+    }
+  }
 }
