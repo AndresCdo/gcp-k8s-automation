03f5cbb8d36fe0d1d237ab2f6bba67700513f5f1
AndresCdo <andrezz1997@gmail.com>
2022-09-27 17:23:45 -0500

    network name from test to default

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ location = var.region
 }
 
 resource "google_compute_network" "default" {
-  name   = "test-network"
+  name   = "default"
 }
 
 resource "google_compute_address" "static" {
@@ -27,7 +27,7 @@ resource "google_compute_address" "static" {
 }
 
 resource "google_compute_subnetwork" "default" {
-  name          = "my-subnet"
+  name          = "default-subnet"
   ip_cidr_range = "10.0.0.0/16"
   region        = var.region
   network       = google_compute_network.default.id
