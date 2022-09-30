b623b7eb60367e8785a1c53f799c97f73680352d
AndresCdo <andrezz1997@gmail.com>
2022-09-30 13:57:52 -0500

    Delete master-node external ip from known_hosts

--- ansible-provider/node-list.sh
+++ ansible-provider/node-list.sh
@@ -3,9 +3,11 @@
 node_list=($(gcloud compute instances list | awk '{ print $4 }' | cat -))
 echo "Master node IP: ${node_list[1]}";
 echo "Worker node IPs: ${node_list[@]:2}";
-echo "$PWD"
+echo ""
 
-for node in ${node_list[@]:1}
+node_ext_ip=($(gcloud compute instances list | grep master | awk '{ print $5 }' | cat -))
+
+for node in ${node_ext_ip[@]}
 do
     ssh-keygen -f "$HOME/.ssh/known_hosts" -R $node
 done
