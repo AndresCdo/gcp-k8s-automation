ef17cc25e3a76b88976c4c4759d518b372399241
AndresCdo <andrezz1997@gmail.com>
2022-10-20 20:44:27 -0500

    Infrastructure of dev.

--- main.tf
+++ main.tf
@@ -16,30 +16,48 @@ provider "google" {
 #   location = var.region
 # }
 
+# Create VPC
 resource "google_compute_network" "default" {
   name   = "default"
 }
 
-resource "google_compute_address" "static" {
-  for_each = toset(var.master_node_name)
-  name     = "ipv4-address-${each.value}"
-  region   = var.region
-}
-
+# Create Subnet
+##  Create public subnet
 resource "google_compute_subnetwork" "default" {
-  name          = "default-subnet"
+  name          = "public-subnet"
   ip_cidr_range = "10.0.0.0/16"
   region        = var.region
   network       = google_compute_network.default.id
 }
 
-# resource "google_compute_address" "internal_with_subnet_and_address" {
-#   name         = "my-internal-address"
-#   subnetwork   = google_compute_subnetwork.default.id
-#   address_type = "INTERNAL"
-#   address      = "10.0.42.42"
-#   region       = var.region
-# }
+## Create private subnet
+resource "google_compute_subnetwork" "internal" {
+  name          = "private-subnet"
+  ip_cidr_range = "10.51.1.0/24"
+  region        = var.region
+  network       = google_compute_network.default.id
+  private_ip_google_access = "true"
+}
+
+resource "google_compute_address" "master-static" {
+  for_each = toset(var.master_node_name)
+  name     = "ipv4-address-${each.value}"
+  region   = var.region
+}
+
+resource "google_compute_address" "worker-static" {
+  for_each = toset(var.worker_node_name)
+  name     = "ipv4-address-${each.value}"
+  region   = var.region
+}
+
+resource "google_compute_address" "internal_with_subnet_and_address" {
+  name         = "private-ip"
+  subnetwork   = google_compute_subnetwork.internal.id
+  address_type = "INTERNAL"
+  address      = "10.51.1.42"
+  region       = var.region
+}
 
 resource "google_compute_firewall" "default" {
   name    = "default-firewall"
@@ -52,7 +70,7 @@ resource "google_compute_firewall" "default" {
 
   allow {
     protocol = "tcp"
-    ports    = ["22", "2022", "80", "443", "8080", "1000-2000", "6443", "6783"]
+    ports    = ["22", "2022", "80", "443", "8080", "1000-2000", "6443", "6783", "10250-10253"]
   }
 
   source_tags = ["web"]
@@ -77,9 +95,9 @@ resource "google_compute_instance" "master" {
   network_interface {
     # A default network is created for all GCP projects
     network    = google_compute_network.default.name
-    subnetwork = google_compute_subnetwork.default.name 
+    subnetwork = google_compute_subnetwork.internal.name 
     access_config {
-      nat_ip = google_compute_address.static["${each.value}"].address
+      nat_ip = google_compute_address.master-static["${each.value}"].address
     }
   }
 
@@ -108,6 +126,9 @@ resource "google_compute_instance" "worker" {
     # A default network is created for all GCP projects
     network    = google_compute_network.default.name
     subnetwork = google_compute_subnetwork.default.name
+    access_config {
+      nat_ip = google_compute_address.worker-static["${each.value}"].address
+    }
   }
 
   lifecycle {
